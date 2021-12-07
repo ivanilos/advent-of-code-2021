e292b80355a738902112b3d021f48fd9a7e97343
ivanilos <ivanilos@gmail.com>
2021-12-07 18:11:11 -0300

    fix day 7 part_two - output was correct but code wasn't

--- 07/part_two.kt
+++ 07/part_two.kt
@@ -1,5 +1,5 @@
 import java.io.File
-import kotlin.math.abs
+import kotlin.math.*
 
 fun readInput() : List<Int> {
     return File("input.txt")
@@ -8,15 +8,27 @@ fun readInput() : List<Int> {
             .map { it.toInt() }
 }
 
-fun solve(positions : List<Int>) : Int {
-    val finalPos = positions.sum() / positions.size
-
+fun movementCost(positions : List<Int>, finalPos : Int) : Int {
     var result = 0
     for (position in positions) {
         val dist = abs(position - finalPos)
         result += ((1 + dist) * dist) / 2
     }
     return result
+}
+
+/*
+The optimal real valued position is at max 0.5 away from the mean
+so it is sufficient to calc for floor(mean) and ceil(mean)
+when the mean is not an integer
+
+https://www.reddit.com/r/adventofcode/comments/rawxad/2021_day_7_part_2_i_wrote_a_paper_on_todays/
+*/
+fun solve(positions : List<Int>) : Int {
+    val meanFloor = positions.sum() / positions.size
+
+    return min(movementCost(positions, meanFloor),
+                    movementCost(positions, meanFloor + 1))
 }
 
 fun main() {
